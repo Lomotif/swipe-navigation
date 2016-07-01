44a0d7fe298d37c2555d8b440ed3478eacbaedb4
Casey <casey@lomotif.com>
2016-07-01 14:36:08 +0800

    add support for non-storyboard

--- swipe-navigation/ViewController.swift
+++ swipe-navigation/ViewController.swift
@@ -16,14 +16,60 @@ enum ActivePanDirection {
 
 class ViewController: UIViewController, EmbeddedViewControllerDelegate, UIGestureRecognizerDelegate {
     
+    // Mark: - Properties
     /*
      * The whole magic to this implementation:
      * manipulation of the x & y constraints of the center container view wrt the BaseViewController's
      *
      * The other (top, bottom, left, right) simply constrain themselves wrt to the center container
      */
-    @IBOutlet weak private var currentXOffset: NSLayoutConstraint!
-    @IBOutlet weak private var currentYOffset: NSLayoutConstraint!
+    @IBOutlet private var currentXOffset: NSLayoutConstraint!
+    @IBOutlet private var currentYOffset: NSLayoutConstraint!
+    
+    // Mark: View controllers
+    private(set) var centerViewController: UIViewController!
+    
+    // Append embedded view to container view's view hierachy
+    var topViewController: UIViewController? {
+        willSet(newValue) {
+            self.shouldshowTopViewController = newValue != nil
+            topViewController?.view.removeFromSuperview()
+            guard let viewController = newValue else {
+                return
+            }
+            addEmbeddedViewController(viewController, position: .Top)
+        }
+    }
+    var bottomViewController: UIViewController? {
+        willSet(newValue) {
+            self.shouldShowBottomViewController = newValue != nil
+            bottomViewController?.view.removeFromSuperview()
+            guard let viewController = newValue else {
+                return
+            }
+            addEmbeddedViewController(viewController, position: .Bottom)
+        }
+    }
+    var leftViewController: UIViewController? {
+        willSet(newValue) {
+            self.shouldShowLeftViewController = newValue != nil
+            leftViewController?.view.removeFromSuperview()
+            guard let viewController = newValue else {
+                return
+            }
+            addEmbeddedViewController(viewController, position: .Left)
+        }
+    }
+    var rightViewController: UIViewController? {
+        willSet(newValue) {
+            self.shouldShowRightviewController = newValue != nil
+            rightViewController?.view.removeFromSuperview()
+            guard let viewController = newValue else {
+                return
+            }
+            addEmbeddedViewController(viewController, position: .Right)
+        }
+    }
     
     // pan gesture recognizer related
     @IBOutlet private var mainPanGesture: UIPanGestureRecognizer!
@@ -41,24 +87,60 @@ class ViewController: UIViewController, EmbeddedViewControllerDelegate, UIGestur
     private var rightContainerOffset: CGVector!
     
     // setting them to NO disables swiping to the view controller, try it!
-    private var shouldshowTopViewController = true
-    private var shouldShowBottomViewController = true
-    private var shouldShowLeftViewController = true
-    private var shouldShowRightviewController = true
+    var shouldshowTopViewController = true
+    var shouldShowBottomViewController = true
+    var shouldShowLeftViewController = true
+    var shouldShowRightviewController = true
     
     private let swipeAnimateDuration = 0.2
     
+    // Mark: - Initializers
+    // Use this initializer if you are not using storyboard
+    init(centerViewController: UIViewController) {
+        super.init(nibName: nil, bundle: nil)
+        shouldshowTopViewController = false
+        shouldShowBottomViewController = false
+        shouldShowLeftViewController = false
+        shouldShowRightviewController = false
+        self.centerViewController = centerViewController
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // Mark: - Functions
     override func viewDidLoad() {
-        // embedded containers offset
-        let frameWidth = view.frame.size.width;
-        let frameHeight = view.frame.size.height;
+        //
+        if currentXOffset == nil || currentYOffset == nil {
+            view.addSubview(centerViewController.view)
+            centerViewController.view.translatesAutoresizingMaskIntoConstraints = false
+            centerViewController.view.backgroundColor = UIColor.blueColor()
+            self.currentXOffset = alignCenterXConstraint(forItem: centerViewController.view, toItem: view, position: .Center)
+            self.currentYOffset = alignCenterYConstraint(forItem: centerViewController.view, toItem: view, position: .Center)
+            view.addConstraints([self.currentXOffset, self.currentYOffset])
+            view.addConstraints(sizeConstraints(forItem: centerViewController.view, toItem: view))
+        }
         
+        if mainPanGesture == nil {
+            mainPanGesture = UIPanGestureRecognizer(target: self, action: #selector(onPanGestureTriggered(_:)))
+            view.addGestureRecognizer(mainPanGesture)
+        }
+        
+        // embedded containers offset
+        let frameWidth = view.frame.size.width
+        let frameHeight = view.frame.size.height
         // bookmark the offsets to specific positions
         centerContainerOffset = CGVectorMake(currentXOffset.constant, currentYOffset.constant)
         topContainerOffset = CGVectorMake(centerContainerOffset.dx, centerContainerOffset.dy + frameHeight)
         bottomContainerOffset = CGVectorMake(centerContainerOffset.dx, centerContainerOffset.dy - frameHeight)
         leftContainerOffset = CGVectorMake(centerContainerOffset.dx + frameWidth, centerContainerOffset.dy)
         rightContainerOffset = CGVectorMake(centerContainerOffset.dx - frameWidth, centerContainerOffset.dy)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -77,14 +159,6 @@ class ViewController: UIViewController, EmbeddedViewControllerDelegate, UIGestur
         // Show navigation bar when navigating away from this view controller.
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
-    
-    override func prefersStatusBarHidden() -> Bool {
-        // required for full screen, apparently
-        return true;
-    }
-    
-    
-    
     
     // MARK: - Containers
     private func showContainer(position: Position) {
@@ -108,9 +182,6 @@ class ViewController: UIViewController, EmbeddedViewControllerDelegate, UIGestur
             self.view.layoutIfNeeded()
         }
     }
-    
-    
-    
     
     // MARK: - EmbeddedViewcontrollerDelegate Conformance
     func isContainerActive(position: Position) -> Bool {
@@ -139,18 +210,14 @@ class ViewController: UIViewController, EmbeddedViewControllerDelegate, UIGestur
         showContainer(position)
     }
     
-    
-    
-    
     // MARK: - Pan Gestures
     // called before touchesBegan:withEvent: is called on the gesture recognizer for a new touch. return NO to prevent the gesture recognizer from seeing this touch
     func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
         return true
     }
     
-    @IBAction private func onPanGestureTriggered(sender: UIPanGestureRecognizer) {
+    @IBAction @objc private func onPanGestureTriggered(sender: UIPanGestureRecognizer) {
         switch sender.state {
-            
         case .Began:
             /* Restrict pan movement
              * - top/bottom not allowed to pan horizontally
@@ -215,7 +282,6 @@ class ViewController: UIViewController, EmbeddedViewControllerDelegate, UIGestur
             sender.setTranslation(CGPointZero, inView: view)
             
         case .Ended:
-            
             /*
              * Handle snapping here
              */
@@ -292,7 +358,6 @@ class ViewController: UIViewController, EmbeddedViewControllerDelegate, UIGestur
                         }
                     }
                 }
-                
                 
             case .Vertical:
                 /* Snap to TOP container (positive y offset)
@@ -376,9 +441,38 @@ class ViewController: UIViewController, EmbeddedViewControllerDelegate, UIGestur
         }
     }
     
+    // Append embedded view to container view's view hierachy
+    func addEmbeddedViewController(viewController: UIViewController, position: Position) {
+        (viewController as? EmbeddedViewController)?.delegate = self
+        view.addSubview(viewController.view)
+        viewController.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraint(alignCenterXConstraint(forItem: viewController.view, toItem: centerViewController.view, position: position))
+        view.addConstraint(alignCenterYConstraint(forItem: viewController.view, toItem: centerViewController.view, position: position))
+        view.addConstraints(sizeConstraints(forItem: viewController.view, toItem: centerViewController.view))
+        view.layoutIfNeeded()
+    }
     
+    // MARK: - Layout Constraints
+    // Create a layout constraint that make view item to align center x to the respective item with offset according to the position
+    // For view item that is positioned on the left will offset by -toItem.width, and +toItem.width if it's positioned on the right
+    func alignCenterXConstraint(forItem item: UIView, toItem: UIView, position: Position) -> NSLayoutConstraint {
+        let offset = position == .Left ? -self.view.frame.width : position == .Right ? toItem.frame.width : 0
+        return NSLayoutConstraint(item: item, attribute: .CenterX, relatedBy: .Equal, toItem: toItem, attribute: .CenterX, multiplier: 1, constant: offset)
+    }
     
+    // Create a layout constraint that make view item to align center y to the respective item height offset according to the position
+    // For view item that is positioned on the top will offset by -toItem.height, and +toItem.height if it's positioned on the right
+    func alignCenterYConstraint(forItem item: UIView, toItem: UIView, position: Position) -> NSLayoutConstraint {
+        let offset = position == .Top ? -self.view.frame.height : position == .Bottom ? toItem.frame.height : 0
+        return NSLayoutConstraint(item: item, attribute: .CenterY, relatedBy: .Equal, toItem: toItem, attribute: .CenterY, multiplier: 1, constant: offset)
+    }
     
+    // Create width and height layout constraints that make make the item.size same as the toItem.size
+    func sizeConstraints(forItem item: UIView, toItem: UIView) -> [NSLayoutConstraint] {
+        let widthConstraint = NSLayoutConstraint(item: item, attribute: .Width, relatedBy: .Equal, toItem: toItem, attribute: .Width, multiplier: 1, constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: item, attribute: .Height, relatedBy: .Equal, toItem: toItem, attribute: .Height, multiplier: 1, constant: 0)
+        return [widthConstraint, heightConstraint]
+    }
     
     /*
      * MARK: - Navigation
